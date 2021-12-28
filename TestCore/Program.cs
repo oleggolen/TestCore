286e98d3ba5ea60ecd861c3fdf81762161d83a63
Oleg Golenischev <oleg-golenischev63@yandex.ru>
2021-12-28 16:16:41 +0300

    Добавлены изменения

--- TestCore/Program.cs
+++ TestCore/Program.cs
@@ -6,7 +6,10 @@ namespace TestCore
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int first = int.Parse(Console.ReadLine() ?? "0");
+            int second = int.Parse(Console.ReadLine() ?? "0");
+            int sum = first + second;
+            Console.WriteLine($"Sum of numbers: {sum}");
         }
     }
 }
